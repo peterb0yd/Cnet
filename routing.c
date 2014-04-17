65f494cd749f8feaa7850113933611538b95fbf3
Peter <peter@peter-Studio-1557.(none)>
2014-04-17 18:11:19 -0400

    Some stuff working

--- routing.c
+++ routing.c
@@ -25,46 +25,45 @@ static EVENT_HANDLER(timeouts)  {
 	// SEND MESSAGE
 	CHECK(CNET_write_physical_reliable(1,&env.msg,&len));
 
-
 	// RESCHEDULE EV_TIMER1 TO OCCUR AGAIN IN 1SEC
    	CNET_start_timer(EV_TIMER1, 1000000, 0);
 }
 
-static EVENT_HANDLER(physical_ready)
- {
-    	Envelope     env;
-    	size_t	 len;
+static void send_reliable()
+{
+	Envelope env = {"World"};
+	size_t  len = 1;
 
-    	len         = sizeof(env);
+    	printf("test");
 
-	// READ MESSAGE
-	CHECK(CNET_read_physical(0, &env.msg, &len));
-	
-	printf("%3d.\t%s\n", &env.msg);
-
+    	printf("sending");
+    	CHECK(CNET_write_physical_reliable(1, &env.msg, &len));
 }
 
-
-static EVENT_HANDLER(application_ready)
+static void physical_ready()
 {
-	CNET_enable_application(ALLNODES);   // window not full
+	Envelope env = {"World"};
+	size_t  len = 1;
+	int link;
+
+   	CHECK(CNET_read_physical(&link, &env.msg, &len));
+
+   	printf("physical\n");
+
 }
 
 
 EVENT_HANDLER(reboot_node)
 {
  	// SET HANDLERS
-    CHECK(CNET_set_handler( EV_APPLICATIONREADY, application_ready, 0));
-    CHECK(CNET_set_handler( EV_PHYSICALREADY,    physical_ready, 0));
+       	CHECK(CNET_set_handler(EV_PHYSICALREADY, physical_ready, 0));
 
-	if(nodeinfo.nodenumber == 0 || nodeinfo.nodenumber == 1) {
-		(void)CNET_enable_application(ALLNODES);
+   	CHECK(CNET_set_handler(EV_DEBUG1, send_reliable, 0));
 
 	//  INDICATE THAT WE ARE INTERESTED IN THE EV_TIMER1 EVENT
-		CHECK(CNET_set_handler(EV_TIMER1, timeouts, 0));
+	CHECK(CNET_set_handler(EV_TIMER1, timeouts, 0));
 
 	//  REQUEST THAT EV_TIMER1 OCCUR IN 1SEC, IGNORING THE RETURN VALUE
-		CNET_start_timer(EV_TIMER1, 1000000, 0);
-	}
+	CNET_start_timer(EV_TIMER1, 1000000, 0);
 }
 
